0f830bb598b6a8557bb0506b9c4ac69b58d2c7f7
chennakeshava1998 <16co108.keshava@nitk.edu.in>
2019-02-08 00:40:00 +0530

    Update run.sh

--- scraper/run.sh
+++ scraper/run.sh
@@ -2,11 +2,11 @@
 
 # Copy to secret.sh and add details there
 # Username, Password not needed for scraping
-export REDDIT_CLIENT_ID="1M4gBu8abP5_hA"
-export REDDIT_CLIENT_SECRET="TTtY7zI3WpAKeCDVhiEJ_h8tQ7U"
-export REDDIT_APP_NAME="profanity-detector"
-export REDDIT_USERNAME="ckisawesome"
-export REDDIT_PASSWORD="praveensucks"
+export REDDIT_CLIENT_ID=""
+export REDDIT_CLIENT_SECRET=""
+export REDDIT_APP_NAME=""
+export REDDIT_USERNAME=""
+export REDDIT_PASSWORD=""
 
 # if [ -z $1 ]; then
 #     echo "Usage: $0 <Search_Query>"
